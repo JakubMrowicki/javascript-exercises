7a44c5af8c83dc12ef9a37a86f26ff964676478f
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-18 20:16:29 +0000

    pass 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
-	
-};
+const add = function (a, b) {
+  return a + b
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (a, b) {
+  return a - b
+}
 
-const sum = function() {
-	
-};
+const sum = function (numbers) {
+  let sum = 0
+  numbers.forEach((num) => (sum += num))
+  return sum
+}
 
-const multiply = function() {
+const multiply = function (numbers) {
+  if (numbers.length === 0) return 0
+  let result = 1
+  numbers.forEach((num) => (result *= num))
+  return result
+}
 
-};
+const power = function (a, b) {
+  return Math.pow(a, b)
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function (num) {
+  if (num < 0) return -1
+  else if (num === 0) return 1
+  else {
+    return num * factorial(num - 1)
+  }
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
-};
+  factorial,
+}
