8f2d0c0005e0a41e0771f788d288190e838c757f
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-18 18:12:11 +0000

    pass 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, amount) {
+  if (amount < 0) {
+    return 'ERROR'
+  }
+  let result = ''
+  for (let i = 0; i < amount; i++) {
+    result += string
+  }
+  return result
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
