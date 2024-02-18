b8cd4b2cdbf88dfbab7582461340bc61431b2353
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-18 18:46:28 +0000

    pass 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
+const sumAll = function (start, end) {
+  if (
+    start < 0 ||
+    end < 0 ||
+    typeof start !== 'number' ||
+    typeof end !== 'number'
+  ) {
+    return 'ERROR'
+  }
+  let result = 0
+  let a = start < end ? start : end
+  const b = end > start ? end : start
+  while (a <= b) {
+    result += a
+    a++
+  }
+  return result
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
