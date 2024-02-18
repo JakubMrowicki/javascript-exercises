f0decc1da71855c8b58345b7a6d9daf278eaba27
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-18 20:25:16 +0000

    pass 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  return cleanString(string) === reverseString(cleanString(string))
+}
 
-};
+const cleanString = (string) => {
+  let lower = string.toLowerCase()
+  let clean = lower.replace(/[^A-Z0-9]/gi, '')
+  return clean
+}
+
+const reverseString = (string) => {
+  result = ''
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i]
+  }
+  return result
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
