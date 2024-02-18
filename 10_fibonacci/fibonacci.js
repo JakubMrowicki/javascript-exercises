23ffbb9a1b06cd372350b079a49487aa554f784d
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-18 20:39:48 +0000

    pass 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (position) {
+  let location = parseInt(position)
+  if (location === 0) return 0
+  if (location < 0) return 'OOPS'
+  let fib = []
+  let i = 0
+  while (fib.length < location) {
+    if (fib.length < 2) fib.push(1)
+    else {
+      const next = fib[i - 1] + fib[i - 2]
+      fib.push(next)
+    }
+    i++
+  }
+  return fib[location - 1]
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
