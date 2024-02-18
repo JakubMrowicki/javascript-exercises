c6898ff78cc9e78768274baedcef5c3d46df2049
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-18 18:32:30 +0000

    pass 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...elements) {
+  let elementsArray = [...elements]
+  elementsArray.forEach((element) => {
+    array = array.filter((item) => item !== element)
+  })
+  return array
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
