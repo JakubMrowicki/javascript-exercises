da574ca8afc8cb6eb62dbd863fb5ddcb32967c18
Jakub Mrowicki <jakubmrowicki96@gmail.com>
2024-02-18 21:23:52 +0000

    pass 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const currYear = new Date(Date.now()).getFullYear()
 
-};
+const findTheOldest = (people) =>
+  people.reduce((oldest, person) => {
+    const personAge = isNumber(person.yearOfDeath)
+      ? person.yearOfDeath - person.yearOfBirth
+      : currYear - person.yearOfBirth
+    const oldestAge = isNumber(oldest.yearOfDeath)
+      ? oldest.yearOfDeath - oldest.yearOfBirth
+      : currYear - oldest.yearOfBirth
+    return personAge > oldestAge ? person : oldest
+  }, people[0])
 
+const isNumber = (value) => (typeof value === 'number' ? true : false)
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
